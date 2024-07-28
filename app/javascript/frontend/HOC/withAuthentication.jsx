cc32b0f6e5abc21364e4bae0c1ceeb70b3a75df3
Steve <stevewu24@gmail.com>
2024-07-27 23:09:16 -0700

    updated withAuthentication  withAuthentication now redirects user if not logged in

--- app/javascript/frontend/HOC/withAuthentication.jsx
+++ app/javascript/frontend/HOC/withAuthentication.jsx
@@ -1,32 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
 const withAuthentication = (WrappedComponent) => {
   const WithAuthentication = (props) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const navigate = useNavigate();
 
     useEffect(() => {
       const checkAuthentication = async () => {
         try {
-          const response = await axios.get('/api/check_authentication');
+          const response = await axios.get('/check_authentication');
           if (response.data.authenticated) {
+            console.log('authenticated');
             setIsAuthenticated(true);
           } else {
-            navigate('/users/sign_up'); // Redirect to login page if not authenticated
+            console.log('not authenticated');
+            window.location.href = '/users/sign_up'; // Redirect to sign up page if not authenticated
           }
         } catch (error) {
           console.error('Error checking authentication:', error);
-          navigate('/users/sign_in');
+          window.location.href = '/users/sign_in'; // Redirect to sign in page on error
         }
       };
 
       checkAuthentication();
-    }, [navigate]);
+    }, []);
 
     if (!isAuthenticated) {
-      return null; // Or render a loading spinner
+      return null;
     }
 
     return <WrappedComponent {...props} />;
