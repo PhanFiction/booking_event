78b281345b6a2e03fb1485f8f73d1370fca137c2
Steve <stevewu24@gmail.com>
2024-08-03 23:33:55 -0700

    updated routes, created amadeus.rb file, created migration file for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   get '/check_authentication', to: 'sessions#check'
+
+  # resources :users do 
+  #   resources :flights
+  # end
+
+  resources :flights, only: [:index]
+  
+  get 'flights/search-flights', to: 'flights#search_flights'
+  get 'flights/flight-offers', to: 'flights#flight_offers'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
