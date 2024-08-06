67244c27220fe41fb11bc622d22406db7d3a76f8
Steve <stevewu24@gmail.com>
2024-08-05 23:18:18 -0700

    created hamburger component

---
+++ app/javascript/frontend/components/Hamburger/Hamburger.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Hamburger() {
+  return (
+    <label class="burger" for="burger">
+      <input type="checkbox" id="burger"/>
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  )
+}
