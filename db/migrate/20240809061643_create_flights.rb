8cbed4913f810fe590fc9a24581d91047a0f7825
Steve <stevewu24@gmail.com>
2024-08-12 16:55:24 -0700

    modified migration file for flights  changed :duration from t.date to t.integer

--- db/migrate/20240809061643_create_flights.rb
+++ db/migrate/20240809061643_create_flights.rb
@@ -2,7 +2,7 @@ class CreateFlights < ActiveRecord::Migration[7.1]
   def change
     create_table :flights do |t|
       t.datetime :date
-      t.time :duration
+      t.integer :duration
       t.references :departure_airport, foreign_key: { to_table: :airports }
       t.references :arrival_airport, foreign_key: { to_table: :airports }
       t.timestamps
