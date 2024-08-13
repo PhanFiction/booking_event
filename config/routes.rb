8ee33586685f18d8340cd92f63085e390cd5dca2
Steve <stevewu24@gmail.com>
2024-08-12 17:00:11 -0700

    updated index.html.erb  removed html elements for showing user is signed in  updated route paths

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   get '/check_authentication', to: 'sessions#check'
+  delete '/logout', to: 'sessions#destroy'
 
-  # resources :users do 
-  #   resources :flights
-  # end
-
-  resources :flights, only: [:index]
+  namespace :api do
+    get 'bookings/index'
+    post 'bookings/create'
+  end
   
-  get 'flights/search-flights', to: 'flights#search_flights'
-  get 'flights/flight-offers', to: 'flights#flight_offers'
+  resources :airports
+  resources :flights, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -21,4 +21,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'homepage#index'
+  
+  # Below is a catch-all route with get '/*path' that will direct any other request that doesn’t match the existing routes to the index action of # the homepage controller. The front-end routing will handle requests unrelated to creating, reading, or deleting
+  # get '/*path' is used to catch all GET requests and route them to a specific controller action which is homepage#index.
+  get '/*path' => 'homepage#index'
 end
